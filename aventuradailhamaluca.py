c733d035c6706d04bd29282ad452d4c2b38959c5
Juan Formaio <juanformaio@gmail.com>
2024-09-30 19:45:09 -0300

    A ilha Maluca

---
+++ aventuradailhamaluca.py
@@ -0,0 +1,296 @@
+import random
+import os , time
+os.system("cls")
+
+print('''
+ __   __       __    __       ___      
+|  | |  |     |  |  |  |     /   \     
+|  | |  |     |  |__|  |    /  ^  \    
+|  | |  |     |   __   |   /  /_\  \   
+|  | |  `----.|  |  |  |  /  _____  \  
+|__| |_______||__|  |__| /__/     \__\ 
+''')
+
+time.sleep(1)
+os.system("cls")
+
+print('''            
+ _______   ______        _______.
+|       \ /  __  \      /       |
+|  .--.  |  |  |  |    |   (----`
+|  |  |  |  |  |  |     \   \    
+|  '--'  |  `--'  | .----)   |   
+|_______/ \______/  |_______/     
+''')
+
+time.sleep(1)
+os.system("cls")
+
+print('''
+.___  ___.      ___       __       __    __    ______   ______        _______.
+|   \/   |     /   \     |  |     |  |  |  |  /      | /  __  \      /       |
+|  \  /  |    /  ^  \    |  |     |  |  |  | |  ,----'|  |  |  |    |   (----`
+|  |\/|  |   /  /_\  \   |  |     |  |  |  | |  |     |  |  |  |     \   \    
+|  |  |  |  /  _____  \  |  `----.|  `--'  | |  `----.|  `--'  | .----)   |   
+|__|  |__| /__/     \__\ |_______| \______/   \______| \______/  |_______/          
+''')
+
+time.sleep(1)
+os.system("cls")
+
+print("BEM-VINDO Á JUMANJI")
+time.sleep(2)
+os.system("cls")
+
+inicio = input("VAMOS JOGAR? Aperte (s) quando estiver pronto e (*) para vazar!: ")
+if inicio == ("s") :
+    print("QUE OS JOGOS COMECEM ! ! !")
+else :
+    quit()
+
+time.sleep(2)
+os.system("cls")
+
+print('''
+    Você é um explorador corajoso em busca de tesouros lendários na misteriosa Ilha Maluca. 
+    Rumores dizem que um grande tesouro está escondido em algum lugar na ilha, 
+    mas para encontrá-lo, você precisa decifrar uma série de enigmas. 
+
+    Ao chegar na ilha, você se depara com uma placa estranha com inscrições antigas. 
+    As inscrições dizem o seguinte: 
+      
+   "Para desbloquear o tesouro escondido, você deve provar sua destreza. 
+    Resolva os desafios a seguir e siga as instruções para encontrar o caminho certo."     
+''')
+input('''                         Aperte ENTER para contnuar                                  ''')
+os.system("cls")
+
+print('''
+
+
+ ██▓███  ▓█████  ███▄    █   ██████ ▓█████     ▄▄▄▄   ▓█████  ███▄ ▄███▓ ▐██▌ 
+▓██░  ██▒▓█   ▀  ██ ▀█   █ ▒██    ▒ ▓█   ▀    ▓█████▄ ▓█   ▀ ▓██▒▀█▀ ██▒ ▐██▌ 
+▓██░ ██▓▒▒███   ▓██  ▀█ ██▒░ ▓██▄   ▒███      ▒██▒ ▄██▒███   ▓██    ▓██░ ▐██▌ 
+▒██▄█▓▒ ▒▒▓█  ▄ ▓██▒  ▐▌██▒  ▒   ██▒▒▓█  ▄    ▒██░█▀  ▒▓█  ▄ ▒██    ▒██  ▓██▒ 
+▒██▒ ░  ░░▒████▒▒██░   ▓██░▒██████▒▒░▒████▒   ░▓█  ▀█▓░▒████▒▒██▒   ░██▒ ▒▄▄  
+▒▓▒░ ░  ░░░ ▒░ ░░ ▒░   ▒ ▒ ▒ ▒▓▒ ▒ ░░░ ▒░ ░   ░▒▓███▀▒░░ ▒░ ░░ ▒░   ░  ░ ░▀▀▒ 
+░▒ ░      ░ ░  ░░ ░░   ░ ▒░░ ░▒  ░ ░ ░ ░  ░   ▒░▒   ░  ░ ░  ░░  ░      ░ ░  ░ 
+░░          ░      ░   ░ ░ ░  ░  ░     ░       ░    ░    ░   ░      ░       ░ 
+            ░  ░         ░       ░     ░  ░    ░         ░  ░       ░    ░    
+                                                    ░                          
+''')
+
+time.sleep(2)
+os.system("cls")
+
+print(''' 
+
+
+ ██▓███   ▒█████   ██▓  ██████      ██████  █    ██  ▄▄▄          ██▒   █▓ ██▓▓█████▄  ▄▄▄                     
+▓██░  ██▒▒██▒  ██▒▓██▒▒██    ▒    ▒██    ▒  ██  ▓██▒▒████▄       ▓██░   █▒▓██▒▒██▀ ██▌▒████▄                   
+▓██░ ██▓▒▒██░  ██▒▒██▒░ ▓██▄      ░ ▓██▄   ▓██  ▒██░▒██  ▀█▄      ▓██  █▒░▒██▒░██   █▌▒██  ▀█▄                 
+▒██▄█▓▒ ▒▒██   ██░░██░  ▒   ██▒     ▒   ██▒▓▓█  ░██░░██▄▄▄▄██      ▒██ █░░░██░░▓█▄   ▌░██▄▄▄▄██                
+▒██▒ ░  ░░ ████▓▒░░██░▒██████▒▒   ▒██████▒▒▒▒█████▓  ▓█   ▓██▒      ▒▀█░  ░██░░▒████▓  ▓█   ▓██▒ ██▓  ██▓  ██▓ 
+▒▓▒░ ░  ░░ ▒░▒░▒░ ░▓  ▒ ▒▓▒ ▒ ░   ▒ ▒▓▒ ▒ ░░▒▓▒ ▒ ▒  ▒▒   ▓▒█░      ░ ▐░  ░▓   ▒▒▓  ▒  ▒▒   ▓▒█░ ▒▓▒  ▒▓▒  ▒▓▒ 
+░▒ ░       ░ ▒ ▒░  ▒ ░░ ░▒  ░ ░   ░ ░▒  ░ ░░░▒░ ░ ░   ▒   ▒▒ ░      ░ ░░   ▒ ░ ░ ▒  ▒   ▒   ▒▒ ░ ░▒   ░▒   ░▒  
+░░       ░ ░ ░ ▒   ▒ ░░  ░  ░     ░  ░  ░   ░░░ ░ ░   ░   ▒           ░░   ▒ ░ ░ ░  ░   ░   ▒    ░    ░    ░   
+             ░ ░   ░        ░           ░     ░           ░  ░         ░   ░     ░          ░  ░  ░    ░    ░  
+                                                                      ░        ░                  ░    ░    ░  
+''')
+
+time.sleep(2)
+os.system("cls")
+
+print(''' 
+      
+
+ ██▓███   ▒█████  ▓█████▄ ▓█████     ▄▄▄       ▄████▄   ▄▄▄       ▄▄▄▄    ▄▄▄       ██▀███   ▐██▌  ▐██▌  ▐██▌    
+▓██░  ██▒▒██▒  ██▒▒██▀ ██▌▓█   ▀    ▒████▄    ▒██▀ ▀█  ▒████▄    ▓█████▄ ▒████▄    ▓██ ▒ ██▒ ▐██▌  ▐██▌  ▐██▌    
+▓██░ ██▓▒▒██░  ██▒░██   █▌▒███      ▒██  ▀█▄  ▒▓█    ▄ ▒██  ▀█▄  ▒██▒ ▄██▒██  ▀█▄  ▓██ ░▄█ ▒ ▐██▌  ▐██▌  ▐██▌    
+▒██▄█▓▒ ▒▒██   ██░░▓█▄   ▌▒▓█  ▄    ░██▄▄▄▄██ ▒▓▓▄ ▄██▒░██▄▄▄▄██ ▒██░█▀  ░██▄▄▄▄██ ▒██▀▀█▄   ▓██▒  ▓██▒  ▓██▒    
+▒██▒ ░  ░░ ████▓▒░░▒████▓ ░▒████▒    ▓█   ▓██▒▒ ▓███▀ ░ ▓█   ▓██▒░▓█  ▀█▓ ▓█   ▓██▒░██▓ ▒██▒ ▒▄▄   ▒▄▄   ▒▄▄     
+▒▓▒░ ░  ░░ ▒░▒░▒░  ▒▒▓  ▒ ░░ ▒░ ░    ▒▒   ▓▒█░░ ░▒ ▒  ░ ▒▒   ▓▒█░░▒▓███▀▒ ▒▒   ▓▒█░░ ▒▓ ░▒▓░ ░▀▀▒  ░▀▀▒  ░▀▀▒    
+░▒ ░       ░ ▒ ▒░  ░ ▒  ▒  ░ ░  ░     ▒   ▒▒ ░  ░  ▒     ▒   ▒▒ ░▒░▒   ░   ▒   ▒▒ ░  ░▒ ░ ▒░ ░  ░  ░  ░  ░  ░    
+░░       ░ ░ ░ ▒   ░ ░  ░    ░        ░   ▒   ░          ░   ▒    ░    ░   ░   ▒     ░░   ░     ░     ░     ░    
+             ░ ░     ░       ░  ░         ░  ░░ ░            ░  ░ ░            ░  ░   ░      ░     ░     ░       ''')
+time.sleep(2)
+os.system("cls")
+
+print("Vamos para o 1° Desafio: O Guardião da Porta")
+
+print('''
+      Você se depara com uma porta enorme guardada por um gigante adormecido. 
+      Para passar pela porta e continuar sua jornada, você precisa responder a seguinte questão: 
+      
+     "Quantos cocos o macaco tem se eu pegar metade dos cocos que ele tem, mais dois cocos, e depois subtrair três cocos?"      
+''')
+
+input('''                                Aperte ENTER para contnuar                                  ''')
+os.system("cls")
+
+cocos = (random.randint(0,100))
+print("O macaco inicialmente tem", cocos, "cocos.")
+
+calculoDesafio1 = (cocos / 2) + 2 - 3 
+
+desafio1 = float(input("Quantos cocos o macaco tem se eu pegar metade dos cocos que ele tem, mais dois cocos, e depois subtrair três cocos?: "))
+
+if calculoDesafio1 == desafio1 :
+    print("PARABÉNS VC PASSOU!!!")
+
+else : 
+    print('''
+
+  ██████ ▓█████      █████▒█    ██ ▓█████▄ ▓█████  █    ██ 
+▒██    ▒ ▓█   ▀    ▓██   ▒ ██  ▓██▒▒██▀ ██▌▓█   ▀  ██  ▓██▒
+░ ▓██▄   ▒███      ▒████ ░▓██  ▒██░░██   █▌▒███   ▓██  ▒██░
+  ▒   ██▒▒▓█  ▄    ░▓█▒  ░▓▓█  ░██░░▓█▄   ▌▒▓█  ▄ ▓▓█  ░██░
+▒██████▒▒░▒████▒   ░▒█░   ▒▒█████▓ ░▒████▓ ░▒████▒▒▒█████▓ 
+▒ ▒▓▒ ▒ ░░░ ▒░ ░    ▒ ░   ░▒▓▒ ▒ ▒  ▒▒▓  ▒ ░░ ▒░ ░░▒▓▒ ▒ ▒ 
+░ ░▒  ░ ░ ░ ░  ░    ░     ░░▒░ ░ ░  ░ ▒  ▒  ░ ░  ░░░▒░ ░ ░ 
+░  ░  ░     ░       ░ ░    ░░░ ░ ░  ░ ░  ░    ░    ░░░ ░ ░ ''')
+    quit()
+
+time.sleep(3)
+os.system("cls")
+
+print("Vamos para o Desafio 2: O Labirinto dos Crocodilos")
+
+print('''
+      Após passar pela porta, você entra em um labirinto infestado de crocodilos famintos. 
+      Para escapar deles, você precisa resolver o seguinte problema: 
+      
+     "Se eu tenho uma corda de 12 metros e preciso atravessar um rio de 7 metros de largura.
+      Quantos metros de corda a mais eu tenho para amarrar nas árvores e atravessar com segurança?"
+''')
+
+input('''                            Aperte ENTER para contnuar                                  ''')
+os.system("cls")
+ 
+desafio2 = float(input('''Se eu tenho uma corda de 12 metros e preciso atravessar um rio de 7 metros de largura. 
+Quantos metros de corda a mais eu tenho para amarrar nas árvores e atravessar com segurança?: '''))
+
+if desafio2 == 5 :
+    print("PARABÉNS VC ESTÁ MAIS PERTO DO SEU PRÊMIO!!!")
+
+else :
+    print('''  
+
+  ██████ ▓█████      █████▒█    ██ ▓█████▄ ▓█████  █    ██ 
+▒██    ▒ ▓█   ▀    ▓██   ▒ ██  ▓██▒▒██▀ ██▌▓█   ▀  ██  ▓██▒
+░ ▓██▄   ▒███      ▒████ ░▓██  ▒██░░██   █▌▒███   ▓██  ▒██░
+  ▒   ██▒▒▓█  ▄    ░▓█▒  ░▓▓█  ░██░░▓█▄   ▌▒▓█  ▄ ▓▓█  ░██░
+▒██████▒▒░▒████▒   ░▒█░   ▒▒█████▓ ░▒████▓ ░▒████▒▒▒█████▓ 
+▒ ▒▓▒ ▒ ░░░ ▒░ ░    ▒ ░   ░▒▓▒ ▒ ▒  ▒▒▓  ▒ ░░ ▒░ ░░▒▓▒ ▒ ▒ 
+░ ░▒  ░ ░ ░ ░  ░    ░     ░░▒░ ░ ░  ░ ▒  ▒  ░ ░  ░░░▒░ ░ ░ 
+░  ░  ░     ░       ░ ░    ░░░ ░ ░  ░ ░  ░    ░    ░░░ ░ ░ ''')
+    quit()
+
+time.sleep(3)
+os.system("cls")
+
+print("E vamos para o Desafio 3: O Enigma Final")
+
+print('''
+      Finalmente, você chega à câmara do tesouro, mas para abri-la, você precisa resolver um enigma final:
+
+      "Se o número de tesouros enterrados na ilha é igual ao dobro do número de palmeiras, 
+       e o número de palmeiras é igual a três vezes o número de periquitos na ilha, 
+       e o número total de periquitos é 42, quantos tesouros tem na ilha?"
+''')
+
+input('''                       Aperte ENTER para contnuar                                  ''')
+os.system("cls")
+
+periquitos = 42
+palmeiras = 3 * periquitos
+tesouros = 2 * palmeiras
+
+desafio3 = float(input('''Se o número de tesouros enterrados na ilha é igual ao dobro do número de palmeiras, 
+e o número de palmeiras é igual a três vezes o número de periquitos na ilha, 
+e o número total de periquitos é 42, quantos tesouros tem na ilha?: '''))
+
+if desafio3 == tesouros :
+    print("WE ARE THE CHAMPIONS, WE ARE THE CHAMPIONS, WE ARE THE CHAMPIONS... OF THE WORLD")
+    quit()
+else : 
+     print('''
+          
+ ██▒   █▓ ▄████▄      ███▄    █  ▄▄▄      ▓█████▄  ▒█████   █    ██ 
+▓██░   █▒▒██▀ ▀█      ██ ▀█   █ ▒████▄    ▒██▀ ██▌▒██▒  ██▒ ██  ▓██▒
+ ▓██  █▒░▒▓█    ▄    ▓██  ▀█ ██▒▒██  ▀█▄  ░██   █▌▒██░  ██▒▓██  ▒██░
+  ▒██ █░░▒▓▓▄ ▄██▒   ▓██▒  ▐▌██▒░██▄▄▄▄██ ░▓█▄   ▌▒██   ██░▓▓█  ░██░
+   ▒▀█░  ▒ ▓███▀ ░   ▒██░   ▓██░ ▓█   ▓██▒░▒████▓ ░ ████▓▒░▒▒█████▓ 
+   ░ ▐░  ░ ░▒ ▒  ░   ░ ▒░   ▒ ▒  ▒▒   ▓▒█░ ▒▒▓  ▒ ░ ▒░▒░▒░ ░▒▓▒ ▒ ▒ 
+   ░ ░░    ░  ▒      ░ ░░   ░ ▒░  ▒   ▒▒ ░ ░ ▒  ▒   ░ ▒ ▒░ ░░▒░ ░ ░ 
+     ░░  ░              ░   ░ ░   ░   ▒    ░ ░  ░ ░ ░ ░ ▒   ░░░ ░ ░ 
+      ░  ░ ░                  ░       ░  ░   ░        ░ ░     ░     
+     ░   ░                                 ░                        
+''')
+     
+time.sleep(1.5)
+os.system("cls")
+
+print('''
+
+███▄    █  ▄▄▄      ▓█████▄  ▒█████   █    ██ 
+ ██ ▀█   █ ▒████▄    ▒██▀ ██▌▒██▒  ██▒ ██  ▓██▒
+▓██  ▀█ ██▒▒██  ▀█▄  ░██   █▌▒██░  ██▒▓██  ▒██░
+▓██▒  ▐▌██▒░██▄▄▄▄██ ░▓█▄   ▌▒██   ██░▓▓█  ░██░
+▒██░   ▓██░ ▓█   ▓██▒░▒████▓ ░ ████▓▒░▒▒█████▓ 
+░ ▒░   ▒ ▒  ▒▒   ▓▒█░ ▒▒▓  ▒ ░ ▒░▒░▒░ ░▒▓▒ ▒ ▒ 
+░ ░░   ░ ▒░  ▒   ▒▒ ░ ░ ▒  ▒   ░ ▒ ▒░ ░░▒░ ░ ░ 
+   ░   ░ ░   ░   ▒    ░ ░  ░ ░ ░ ░ ▒   ░░░ ░ ░ 
+         ░       ░  ░   ░        ░ ░     ░     
+                      ░                          
+''')
+
+time.sleep(1)
+os.system("cls")
+
+print('''     
+
+ ███▄    █  ▄▄▄      ▓█████▄  ▒█████   █    ██                
+ ██ ▀█   █ ▒████▄    ▒██▀ ██▌▒██▒  ██▒ ██  ▓██▒               
+▓██  ▀█ ██▒▒██  ▀█▄  ░██   █▌▒██░  ██▒▓██  ▒██░               
+▓██▒  ▐▌██▒░██▄▄▄▄██ ░▓█▄   ▌▒██   ██░▓▓█  ░██░               
+▒██░   ▓██░ ▓█   ▓██▒░▒████▓ ░ ████▓▒░▒▒█████▓  ██▓  ██▓  ██▓ 
+░ ▒░   ▒ ▒  ▒▒   ▓▒█░ ▒▒▓  ▒ ░ ▒░▒░▒░ ░▒▓▒ ▒ ▒  ▒▓▒  ▒▓▒  ▒▓▒ 
+░ ░░   ░ ▒░  ▒   ▒▒ ░ ░ ▒  ▒   ░ ▒ ▒░ ░░▒░ ░ ░  ░▒   ░▒   ░▒  
+   ░   ░ ░   ░   ▒    ░ ░  ░ ░ ░ ░ ▒   ░░░ ░ ░  ░    ░    ░   
+         ░       ░  ░   ░        ░ ░     ░       ░    ░    ░  
+                      ░                          ░    ░    ░    
+''')
+
+time.sleep(1)
+os.system("cls")
+
+print('''
+           
+ ███▄ ▄███▓ ▄▄▄        ██████  ▐██▌  ▐██▌  ▐██▌ 
+▓██▒▀█▀ ██▒▒████▄    ▒██    ▒  ▐██▌  ▐██▌  ▐██▌ 
+▓██    ▓██░▒██  ▀█▄  ░ ▓██▄    ▐██▌  ▐██▌  ▐██▌ 
+▒██    ▒██ ░██▄▄▄▄██   ▒   ██▒ ▓██▒  ▓██▒  ▓██▒ 
+▒██▒   ░██▒ ▓█   ▓██▒▒██████▒▒ ▒▄▄   ▒▄▄   ▒▄▄  
+░ ▒░   ░  ░ ▒▒   ▓▒█░▒ ▒▓▒ ▒ ░ ░▀▀▒  ░▀▀▒  ░▀▀▒ 
+░  ░      ░  ▒   ▒▒ ░░ ░▒  ░ ░ ░  ░  ░  ░  ░  ░ 
+░      ░     ░   ▒   ░  ░  ░      ░     ░     ░ 
+       ░         ░  ░      ░   ░     ░     ░      
+''')
+
+time.sleep(1)
+os.system("cls")
+
+print('''
+
+  ██████ ▓█████      █████▒█    ██ ▓█████▄ ▓█████  █    ██  ▐██▌  ▐██▌  ▐██▌ 
+▒██    ▒ ▓█   ▀    ▓██   ▒ ██  ▓██▒▒██▀ ██▌▓█   ▀  ██  ▓██▒ ▐██▌  ▐██▌  ▐██▌ 
+░ ▓██▄   ▒███      ▒████ ░▓██  ▒██░░██   █▌▒███   ▓██  ▒██░ ▐██▌  ▐██▌  ▐██▌ 
+  ▒   ██▒▒▓█  ▄    ░▓█▒  ░▓▓█  ░██░░▓█▄   ▌▒▓█  ▄ ▓▓█  ░██░ ▓██▒  ▓██▒  ▓██▒ 
+▒██████▒▒░▒████▒   ░▒█░   ▒▒█████▓ ░▒████▓ ░▒████▒▒▒█████▓  ▒▄▄   ▒▄▄   ▒▄▄  
+▒ ▒▓▒ ▒ ░░░ ▒░ ░    ▒ ░   ░▒▓▒ ▒ ▒  ▒▒▓  ▒ ░░ ▒░ ░░▒▓▒ ▒ ▒  ░▀▀▒  ░▀▀▒  ░▀▀▒ 
+░ ░▒  ░ ░ ░ ░  ░    ░     ░░▒░ ░ ░  ░ ▒  ▒  ░ ░  ░░░▒░ ░ ░  ░  ░  ░  ░  ░  ░ 
+░  ░  ░     ░       ░ ░    ░░░ ░ ░  ░ ░  ░    ░    ░░░ ░ ░     ░     ░     ░ 
+      ░     ░  ░             ░        ░       ░  ░   ░      ░     ░     ░      
+''')
+quit()
